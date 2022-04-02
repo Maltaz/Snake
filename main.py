312912c3bcf6603d95236e8b0a79ad4bba35fc86
Marek Napieracz <developer@datacose.com>
2022-04-02 12:29:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import time
+import scoreboard
+
+from snake import Snake
+from food import Food
+from turtle import Screen, Turtle
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Scoreboard()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food
+    if snake.head.distance(food) < 18:
+        food.refresh()
+        snake.extend()
+        score.increase()
+
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        score.reset()
+
+    #Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            score.reset()
+            snake.reset()
+
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,42 @@
+from turtle import Turtle
+
+ALIGMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 250)
+        self.speed("fastest")
+        self.write(f"Score {self.score}", align=ALIGMENT, font=(FONT, 24, "normal"))
+
+    def update_scoreboard(self):
+        """Docstring"""
+        self.clear()
+        self.write(f"Score {self.score} High score {self.high_score}", align=ALIGMENT, font=(FONT, 24, "normal"))
+
+    def reset(self):
+        """Docstring"""
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase(self):
+        """Docstring"""
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+
+
